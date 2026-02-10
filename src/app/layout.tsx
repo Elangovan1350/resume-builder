75c6aac3c9e533a3a761fba9e63a57bf41542398
Elangovan1350 <elangovan2019miss@gmail.com>
2026-02-10 20:23:03 +0530

    change22

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        {/* <Theme /> */}
+        <Theme />
         {children}
       </body>
     </html>
